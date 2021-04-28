5e3df853e66a2c7d520b752f00c451a9f78f0350
ali.abukahil <ali.abukahil@gmail.com>
2021-04-28 11:54:42 +0200

    Updates

---
+++ js/index.js
@@ -0,0 +1,105 @@
+let chars = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "@",
+  "#",
+  "!",
+  ".",
+  "&",
+  "$",
+];
+
+let exclure = ["azerty", "qwerty"];
+
+function generer() {
+  document.getElementById("add").value = main();
+}
+
+function randomize() {
+  let index = Math.floor(Math.random() * 68);
+  return index;
+}
+
+function generate() {
+  let word =
+    chars[randomize()] +
+    chars[randomize()] +
+    chars[randomize()] +
+    chars[randomize()] +
+    chars[randomize()] +
+    chars[randomize()] +
+    chars[randomize()] +
+    chars[randomize()];
+  return word;
+}
+
+function main() {
+  let result = "";
+  let trials = "";
+  let counter = 0;
+  do {
+    result = generate();
+    trials += result;
+  } while (exclure.indexOf(result) >= 0);
+  return trials;
+}
